bf4a99549f27e3cae2e43ed76546708adcc18692
Leonardo <leo.moleiromurcio@gmail.com>
2023-03-12 09:37:12 -0300

    Exibição do maior valor

---
+++ AtividadeMaiorValor.py
@@ -0,0 +1,7 @@
+valorA = float(input("Digite o Valor A: "))
+valorB = float(input("Digite o Valor B: "))
+
+if valorA > valorB:
+    print("O valor maior é o: ", valorA)
+else:
+    print("O valor maior é o: ", valorB)
